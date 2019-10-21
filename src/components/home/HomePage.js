9dc571ec23122c0d1c378b2a8dbf7e39b26a2e5f
seth@mondido <sethnejame>
2019-10-21 19:44:12 +0200

    Adding content to Home page

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const HomePage = () => {
-  return (
-    <div>
-      <h2>Home</h2>
-    </div>
-  );
-};
+const HomePage = () => (
+  <div className="jumbotron">
+    <h1>React Redux Review</h1>
+    <p>React, Redux and React Router for responsive web applications.</p>
+    <Link to="about" className="btn btn-primary btn-lg">About</Link>
+  </div>
+);
 
 export default HomePage;
