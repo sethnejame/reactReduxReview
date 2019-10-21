677400831f8681917d7717ce9c7e574d04b2b591
seth@mondido <sethnejame>
2019-10-21 20:04:05 +0200

    Wiring up Header w/ App

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,11 @@ import React from "react";
 import { Route } from "react-router-dom";
 import HomePage from "./home/HomePage";
 import AboutPage from "./about/AboutPage";
+import Header from "./common/Header";
 
 const App = () => {
   return (
+    <Header />
     <div className="container-fluid">
       <Route exact path="/" component={HomePage} />
       <Route path="/about" component={AboutPage} />
