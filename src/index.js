28cb4f940dee9f80396791abe6eb08d40580b959
seth@mondido <sethnejame>
2019-10-21 19:51:37 +0200

    Create/import index.css for basic styling

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { render } from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css"; //imports the minified vers of BS
 import App from "./components/App";
+import "./index.css";
 
 render(
   <Router>
