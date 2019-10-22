236eb20724dd9c5e02b00cf0817f87d6bdc80942
seth@mondido <sethnejame>
2019-10-22 06:55:30 +0200

    Adding course form @ CoursesPage, bootstrap styling, change handler

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,19 +1,35 @@
 import React from "react";
 
 class CoursesPage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      course: {
-        title: ""
-      }
-    };
-  }
+  state = {
+    course: {
+      title: ""
+    }
+  };
+
+  handleChange = e => {
+    const course = { ...this.state.course, title: e.target.value };
+    this.setState({ course });
+  };
 
   render() {
     return (
       <div>
-        <h2>Courses</h2>
+        <form>
+          <h2>Courses</h2>
+          <h3>Add Course</h3>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              onChange={this.handleChange}
+              value={this.state.course.title}
+            />
+          </div>
+          <div className="form-group">
+            <input className="btn btn-primary" type="submit" value="save" />
+          </div>
+        </form>
       </div>
     );
   }
