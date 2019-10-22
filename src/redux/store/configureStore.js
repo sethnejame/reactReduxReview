823592a15f8656dd37716a4444888ca88b3d4261
seth@mondido <sethnejame>
2019-10-22 07:34:49 +0200

    Adding configStore file to init redux store

---
+++ src/redux/store/configureStore.js
@@ -0,0 +1,18 @@
+// store gets config'd when our app starts
+// redux middleware provides extra functionality
+// compose adds support redux devtools
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducer from "./reducers";
+
+// warns us if someone (or something) tries to mutate state
+import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
+
+export default function configureStore(initialState) {
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  return createStore(
+    rootReducer,
+    initialState,
+    composeEnhancers(applyMiddleware(reduxImmutableStateInvariant()))
+  );
+}
