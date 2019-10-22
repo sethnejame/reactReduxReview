613b1876fcc90e00af7b35289dc1b7c2e3897b31
seth@mondido <sethnejame>
2019-10-22 07:35:37 +0200

    Fixing rootReducer import @ store

--- src/redux/store/configureStore.js
+++ src/redux/store/configureStore.js
@@ -2,7 +2,7 @@
 // redux middleware provides extra functionality
 // compose adds support redux devtools
 import { createStore, applyMiddleware, compose } from "redux";
-import rootReducer from "./reducers";
+import rootReducer from "../reducers";
 
 // warns us if someone (or something) tries to mutate state
 import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
