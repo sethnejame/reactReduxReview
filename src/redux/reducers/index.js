501779819c1a9b94428110266c8b119e81ddb80a
Seth NeJame <seth@worldfavor.com>
2019-12-06 07:20:43 +0100

    Cleaning up rootReducer for courses

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,12 +1,8 @@
 import { combineReducers } from "redux";
-// below we import courseReducer as 'courses'
-import courses from "./courseReducer";
+import {courseReducer} from './courseReducer'
 
 const rootReducer = combineReducers({
-  // and then we combine our courseReducer (courses)
-  // this would normally be courses: courseReducer
-  // but we are using object shorthand so it is now 'courses'
-  courses
+  courses: courseReducer
 });
 
 export default rootReducer;
