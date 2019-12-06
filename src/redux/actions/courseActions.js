d8d7c67ce69803df893fc8fd95f9ef5fd3a47e11
Seth NeJame <seth@worldfavor.com>
2019-12-06 07:14:41 +0100

    Fixing courseReducer (conv to const, clean up naming conventions)

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,3 +1,3 @@
 export function createCourse(course) {
-  return { type: 'CREATE_COURSE', course: course}
+  return { type: 'CREATE_COURSE', payload: course}
 }
--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -1,11 +1,8 @@
-// below we use an array because the state will  be an array of courses
-export default function courseReducer(state = [], action) {
+export const courseReducer = (state = [], action) => {
   switch(action.type) {
     case "CREATE_COURSE":
-      // again, spreading the old list of courses into an array
-      // and adding the new one so we don't mutate old state
-      return [...state, {...action.course}]
+      return [...state, {...action.payload}];
     default:
       return state;
   }
-}
+};
