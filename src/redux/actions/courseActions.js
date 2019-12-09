2eabd5a1a5b4858d2a698cca7000222ca29c6eb3
Seth NeJame <seth@worldfavor.com>
2019-12-09 09:21:40 +0100

    courseActions now uses constants @ actionTypes

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,5 +1,7 @@
+import { CREATE_COURSE } from "./actionTypes";
+
 export function createCourse(course) {
   debugger;
   console.log('createCourse action is called');
-  return { type: 'CREATE_COURSE', payload: course}
+  return { type: CREATE_COURSE, payload: course}
 }
