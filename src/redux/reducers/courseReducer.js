b167b2bff1e0d2cfbbca4de86553b0f26a60e013
Seth NeJame <seth@worldfavor.com>
2019-12-09 09:22:59 +0100

    courseReducer now uses constants @ switcher

--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -1,6 +1,8 @@
+import { CREATE_COURSE } from "../actions/actionTypes";
+
 export const courseReducer = (state = [], action) => {
   switch(action.type) {
-    case "CREATE_COURSE":
+    case CREATE_COURSE:
       debugger;
       console.log('courseReducer invokes the action');
       return [...state, {...action.payload}];
