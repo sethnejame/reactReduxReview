10116d18ccf902f94a79a340f2ee9e408139733e
Seth NeJame <seth@worldfavor.com>
2019-12-13 07:23:31 +0100

    Wiring up loadCourses to CoursesPage, course list now loads on component mount

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,11 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
 import PropTypes from "prop-types";
 
 const CoursesPage = (props) => {
 
-  const { courses } = props;
+  const { courses, loadCourses } = props;
+
+  useEffect(() => {
+    loadCourses()
+  }, []);
 
   return (
       <>
@@ -23,19 +27,19 @@ const CoursesPage = (props) => {
 
 CoursesPage.propTypes = {
   createCourse: PropTypes.func.isRequired,
+  loadCourses: PropTypes.func.isRequired,
   courses: PropTypes.array.isRequired
 };
 // the first argument in mapStateToProps is the entire Redux store state (state)
 function mapStateToProps(state) {
-  debugger;
-  console.log('mapState is called, mapping the new stored state to the CoursesPage component (passed in on props)');
   return {
     courses: state.allCourses
   };
 }
 
 const mapDispatchToProps = {
-  createCourse: courseActions.createCourse
+  createCourse: courseActions.createCourse,
+  loadCourses: courseActions.loadCourses,
 };
 
 export default connect(mapStateToProps,mapDispatchToProps)(CoursesPage);
--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -1,11 +1,13 @@
-import { CREATE_COURSE } from "../actions/actionTypes";
+import { CREATE_COURSE, LOAD_COURSES_SUCCESS } from "../actions/actionTypes";
 
 export const courseReducer = (state = [], action) => {
   switch(action.type) {
     case CREATE_COURSE:
-      debugger;
-      console.log('courseReducer invokes the action');
+      console.log('courseReducer invokes createCourse');
       return [...state, {...action.payload}];
+    case LOAD_COURSES_SUCCESS:
+      console.log('courseReducer invokes loadCourses');
+      return action.payload;
     default:
       return state;
   }
