6020d1c1d6a914f6acc6b871e0c6ea51b90925db
Seth NeJame <seth@worldfavor.com>
2019-12-13 06:57:02 +0100

    Adding loadCourses to courseActions

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,7 +1,23 @@
-import { CREATE_COURSE } from "./actionTypes";
+import { CREATE_COURSE, LOAD_COURSES_SUCCESS } from "./actionTypes";
+import * as courseApi from "../../api/courseApi"
 
 export function createCourse(course) {
-  debugger;
   console.log('createCourse action is called');
-  return { type: CREATE_COURSE, payload: course}
+  return { type: CREATE_COURSE, payload: course }
+}
+
+export function loadCoursesSuccess(courses) {
+  return { type: LOAD_COURSES_SUCCESS, payload: courses }
+}
+// thunks go at bottom of file
+// every thunk returns a func which accepts dispatch
+export function loadCourses() {
+  return function(dispatch) {
+    return courseApi.getCourses().then(courses => {
+      dispatch(loadCoursesSuccess(courses));
+    }).catch(error => {
+      console.log(error);
+      throw error;
+    })
+  }
 }
