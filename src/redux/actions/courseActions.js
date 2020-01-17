449a33203a5e0183ddb240ba29212bc31260def9
Seth <sethnejame@gmail.com>
2020-01-17 07:24:58 +0100

    Adding functionality to update and create @ courses reducer

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,10 +1,6 @@
 import * as types from "./actionTypes";
 import * as courseApi from "../../api/courseApi"
 
-export function createCourse(course) {
-  return { type: types.CREATE_COURSE, payload: course }
-}
-
 export function loadCoursesSuccess(courses) {
   return { type: types.LOAD_COURSES_SUCCESS, payload: courses }
 }
--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -3,14 +3,12 @@ import initialState from './initialState'
 
 export const courseReducer = (state = initialState.courses, action) => {
   switch(action.type) {
-    case types.CREATE_COURSE:
-      return [...state, {...action.payload}];
     case types.LOAD_COURSES_SUCCESS:
       return action.payload;
     case types.UPDATED_COURSE_SUCCESS:
-      //TODO
+      return state.map(course => course.id === action.course.id ? action.course : course)
     case types.CREATE_COURSE_SUCCESS:
-      //TODO
+      return [...state, {...action.payload}];
     default:
       return state;
   }
