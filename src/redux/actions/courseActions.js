564af78c34042c2373e88b4d7347dfd1c30a7375
Seth <sethnejame@gmail.com>
2020-01-17 07:18:01 +0100

    Adding new course action types (update & create) and adding saveCourse thunk w/ corresponding reducer actions

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,12 +1,20 @@
-import { CREATE_COURSE, LOAD_COURSES_SUCCESS } from "./actionTypes";
+import * as types from "./actionTypes";
 import * as courseApi from "../../api/courseApi"
 
 export function createCourse(course) {
-  return { type: CREATE_COURSE, payload: course }
+  return { type: types.CREATE_COURSE, payload: course }
 }
 
 export function loadCoursesSuccess(courses) {
-  return { type: LOAD_COURSES_SUCCESS, payload: courses }
+  return { type: types.LOAD_COURSES_SUCCESS, payload: courses }
+}
+
+export function updatedCourseSuccess(courses) {
+  return { type: types.UPDATED_COURSE_SUCCESS, payload: courses }
+}
+
+export function createCourseSuccess(courses) {
+  return { type: types.CREATE_COURSE_SUCCESS, payload: courses }
 }
 // thunks go at bottom of file
 // every thunk returns a func which accepts dispatch
@@ -14,6 +22,21 @@ export function loadCourses() {
   return function(dispatch) {
     return courseApi.getCourses().then(courses => {
       dispatch(loadCoursesSuccess(courses));
+    }).catch(error => {
+      console.log('There was an error: ' + error);
+      throw error;
+    })
+  }
+}
+
+export function saveCourse(course) {
+  return function(dispatch, getState) {
+    return courseApi
+      .saveCourse(course)
+      .then(savedCourse => {
+        course.id
+          ? dispatch(updatedCourseSuccess(savedCourse))
+          : dispatch(createCourseSuccess(savedCourse))
     }).catch(error => {
       console.log('There was an error: ' + error);
       throw error;
